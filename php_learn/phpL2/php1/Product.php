949612528072c4094e3bbef29c69e313492a91e0
Nikita Ronzin <Nexux93@yandex.ru>
2019-07-04 11:46:55 +0300

    решение первого задания по классам

---
+++ php_learn/phpL2/php1/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Product
+{
+    protected $name;
+    protected $price;
+    protected $description;
+
+    public function product_get_price()
+    {
+
+    }
+    public function product_get_name(){
+
+    }
+
+    public function product_get_description() {
+
+    }
+}
